c0724757978d0404c6bb39e7bc8d29f5da803059
Dustin Sallings <dustin@spy.net>
2014-11-01 20:35:13 -0700

    Do proper error handling in the example

--- examples/client/exampleclient.go
+++ examples/client/exampleclient.go
@@ -47,17 +47,20 @@ func main() {
 	n, id, r, err := c.Head(strconv.FormatInt(g.High-1, 10))
 	maybefatal("getting head", err)
 	log.Printf("msg %d has id %v and the following headers", n, id)
-	io.Copy(os.Stdout, r)
+	_, err = io.Copy(os.Stdout, r)
+	maybefatal("reading head", err)
 
 	n, id, r, err = c.Body(strconv.FormatInt(n, 10))
 	maybefatal("getting body", err)
 	log.Printf("Body of message %v", id)
 	io.Copy(os.Stdout, r)
+	maybefatal("reading body", err)
 
 	n, id, r, err = c.Article(strconv.FormatInt(n, 10))
 	maybefatal("getting the whole thing", err)
 	log.Printf("Full message %v", id)
 	io.Copy(os.Stdout, r)
+	maybefatal("reading the full message", err)
 
 	err = c.Post(strings.NewReader(examplepost))
 	maybefatal("posting", err)
